e51977ac9d404e50489385e889450b0b32f45a03
Anas Alaa Abdo Ahmed <anas1anas2anas3anas4anas56789@gmail.com>
2026-06-28 23:21:31 +0300

    Add: valid-tree - submission-2

---
+++ Data Structures & Algorithms/valid-tree/submission-2.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    void un(vector<int>&v,int i, int j){
+        if(v[i]<v[j]){
+            v[i]=v[i]+v[j];
+            v[j]=i;
+        }
+        else{
+            v[j]=v[i]+v[j];
+            v[i]=j;
+        }
+    }
+    int find(vector<int>& v, int i) {
+    if (v[i] < 0) 
+        return i;
+    return v[i] = find(v, v[i]); 
+    }
+    bool validTree(int n, vector<vector<int>>& edges) {
+        if(edges.size()!=n-1) return false;
+        vector<int>ve (n,-1);
+        int i=0;
+        int u,v;
+        while (i<n-1){
+            u=edges[i][0];
+            v=edges[i][1];
+            if(find(ve,u)==find(ve,v))
+                return false;
+            un(ve,find(ve,u),find(ve,v));
+            i++;
+        }
+        return true;
+    }
+};
